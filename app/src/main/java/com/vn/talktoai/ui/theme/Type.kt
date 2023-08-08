7a119fad5d61a28059428ce6427fad8d094834b6
vladimirtarasov <vptarasov1998@gmail.com>
2023-08-08 21:13:34 +0300

    Add custom fonts

--- app/src/main/java/com/vn/talktoai/ui/theme/Type.kt
+++ app/src/main/java/com/vn/talktoai/ui/theme/Type.kt
@@ -2,14 +2,16 @@ package com.vn.talktoai.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.vn.talktoai.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(listOf(Font(R.font.robotomono_regular), Font(R.font.robotomono_italic))),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
