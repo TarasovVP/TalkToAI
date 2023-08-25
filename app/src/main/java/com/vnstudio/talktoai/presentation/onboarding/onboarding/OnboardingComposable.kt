6bf053d7d4e4349cc3a53b774b4bacb7ae192522
vladimirtarasov <vptarasov1998@gmail.com>
2023-08-25 10:48:11 +0300

    Implement onboarding screen

--- app/src/main/java/com/vnstudio/talktoai/presentation/onboarding/onboarding/OnboardingComposable.kt
+++ app/src/main/java/com/vnstudio/talktoai/presentation/onboarding/onboarding/OnboardingComposable.kt
@@ -1,12 +1,21 @@
 package com.vnstudio.talktoai.presentation.onboarding.onboarding
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.vnstudio.talktoai.R
 import com.vnstudio.talktoai.presentation.components.PrimaryButton
+import com.vnstudio.talktoai.ui.theme.Neutral50
+import com.vnstudio.talktoai.ui.theme.Primary500
 
 @Composable
 fun OnboardingScreen(onNextScreen: () -> Unit, ) {
@@ -32,10 +41,32 @@ fun OnboardingScreen(onNextScreen: () -> Unit, ) {
 @Composable
 fun OnboardingPage(page: Int, onClick: () -> Unit, ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Page $page")
-        PrimaryButton(text = if (page == 3) "Enter" else "Next", modifier = Modifier, onClick = onClick)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Primary500, shape = RoundedCornerShape(16.dp))
+        ) {
+            Text(text = "Привет, Я - Искусственный Интеллект. Page $page", textAlign = TextAlign.Center, color = Neutral50, modifier = Modifier
+                .fillMaxWidth().padding(16.dp))
+        }
+        Image(painter = painterResource(id = R.drawable.onboarding_intro), contentDescription = "Onboarding icon", modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp))
+        Image(painter = painterResource(id = when(page) {
+            1 -> R.drawable.ic_tab_two
+            2 -> R.drawable.ic_tab_three
+            3 -> R.drawable.ic_tab_four
+            else -> R.drawable.ic_tab_one
+        }), contentDescription = "Onboarding tab $page", modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp))
+        PrimaryButton(text = if (page == 3) "Войти" else "Дальше", modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp), onClick = onClick)
     }
 }
