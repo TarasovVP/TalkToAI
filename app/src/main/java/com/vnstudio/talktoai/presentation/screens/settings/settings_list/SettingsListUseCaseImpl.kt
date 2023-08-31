7ea12a869b31fd9ae0c58993e925d96f06521255
vladimirtarasov <vptarasov1998@gmail.com>
2023-08-31 13:39:33 +0300

    Improve settings drawer mode

--- app/src/main/java/com/vnstudio/talktoai/presentation/screens/settings/settings_list/SettingsListUseCaseImpl.kt
+++ app/src/main/java/com/vnstudio/talktoai/presentation/screens/settings/settings_list/SettingsListUseCaseImpl.kt
@@ -3,21 +3,14 @@ package com.vnstudio.talktoai.presentation.screens.settings.settings_list
 import com.vnstudio.talktoai.domain.sealed_classes.Result
 import com.vnstudio.talktoai.domain.models.Feedback
 import com.vnstudio.talktoai.domain.repositories.AuthRepository
-import com.vnstudio.talktoai.domain.repositories.DataStoreRepository
 import com.vnstudio.talktoai.domain.repositories.RealDataBaseRepository
 import com.vnstudio.talktoai.domain.usecases.SettingsListUseCase
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class SettingsListUseCaseImpl @Inject constructor(
-    private val dataStoreRepository: DataStoreRepository,
     private val realDataBaseRepository: RealDataBaseRepository,
     private val authRepository: AuthRepository
 ): SettingsListUseCase {
-
-    override suspend fun getAppLanguage(): Flow<String?> {
-        return dataStoreRepository.getAppLang()
-    }
 
     override fun currentUserEmail() = authRepository.currentUserEmail()
 
