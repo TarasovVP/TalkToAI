b2f06cdf1f5da9f32ee5b3c256f467fc532914ee
vladimirtarasov <vptarasov1998@gmail.com>
2024-01-21 20:27:31 +0200

    Change show hide button approach

--- app/src/main/java/com/vnstudio/talktoai/presentation/components/TextFields.kt
+++ app/src/main/java/com/vnstudio/talktoai/presentation/components/TextFields.kt
@@ -4,35 +4,34 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vnstudio.talktoai.CommonExtensions.EMPTY
-import com.vnstudio.talktoai.CommonExtensions.isNotNull
 import com.vnstudio.talktoai.R
 import com.vnstudio.talktoai.presentation.theme.Neutral600
 import com.vnstudio.talktoai.presentation.theme.Primary500
@@ -135,7 +134,10 @@ fun TextFieldWithButton(
     val focusManager = LocalFocusManager.current
     LaunchedEffect(inputValue.value) {
         if (inputValue.value.text.isEmpty()) focusManager.clearFocus()
-        Log.e("inputValueTAG", "TextFields TextFieldWithButton LaunchedEffect inputValue ${inputValue.value.text}")
+        Log.e(
+            "inputValueTAG",
+            "TextFields TextFieldWithButton LaunchedEffect inputValue ${inputValue.value.text}"
+        )
     }
 
     Log.e("inputValueTAG", "TextFields TextFieldWithButton inputValue ${inputValue.value.text}")
@@ -169,7 +171,10 @@ fun TextFieldWithButton(
             IconButton(enabled = isEnabled && inputValue.value.text.isNotBlank(), onClick = {
                 onSendClick.invoke(inputValue.value.text.trim())
                 inputValue.value = TextFieldValue(String.EMPTY)
-                Log.e("inputValueTAG", "TextFields TextFieldWithButton onClick inputValue ${inputValue.value.text}")
+                Log.e(
+                    "inputValueTAG",
+                    "TextFields TextFieldWithButton onClick inputValue ${inputValue.value.text}"
+                )
             }) {
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.ic_message_send),
@@ -185,57 +190,29 @@ fun TextFieldWithButton(
 fun TruncatableText(
     message: String,
     isTruncated: MutableState<Boolean>,
-    linesCount: Int
+    linesCount: Int,
 ) {
-    Log.e("truncateTAG", "TruncatableText init message ${message.takeIf { it.length > 6 }?.substring(0, 6) } isTruncated ${isTruncated.value}" )
-    if ( linesCount > 2) {
-        val annotatedString = buildAnnotatedString {
-            val textSpanStyle = MaterialTheme.typography.body1.toSpanStyle().copy(color = Color.White)
-            withStyle(style = textSpanStyle) {
-                append(message)
-            }
-            val truncationButton = if (isTruncated.value) " More " else " Hide "
-            withStyle(style = textSpanStyle.copy(color = Color.Blue, fontStyle = androidx.compose.ui.text.font.FontStyle.Italic, fontSize = 12.sp)) {
-                append( truncationButton )
-                addStringAnnotation(
-                    tag = "CLICKABLE",
-                    annotation = "icon",
-                    start = length - truncationButton.length,
-                    end = length
-                )
-            }
-        }
-
-        ClickableText(
-            text = annotatedString,
-            modifier = Modifier
-                .padding(10.dp)
-                .wrapContentSize()
-                .pointerInput(Unit) {
-                    Log.e("clickTAG", "pointerInput: ")
-                    detectTapGestures(onTap = {
-                        Log.e("clickTAG", "pointerInput: ")
-                    })
-                    detectTapGestures(onLongPress = {
-                        Log.e("clickTAG", "pointerInput: ")
-                    })
-                }
-                .let { if (isTruncated.value) it.heightIn(max = 68.dp) else it },
-            onClick = { offset ->
-                Log.e("truncateTAG", "TruncatableText onClick message ${message.takeIf { it.length > 6 }?.substring(0, 6) } isTruncated ${isTruncated.value}" )
-                annotatedString.getStringAnnotations(tag = "CLICKABLE", start = offset, end = offset).firstOrNull()?.let {
-                    isTruncated.value = isTruncated.value.not()
-                }
-            }
-        )
-    } else {
+    Column(modifier = Modifier
+        .padding(10.dp)
+        .wrapContentSize()) {
         Text(
             text = message,
             fontSize = getDimensionResource(resId = R.dimen.default_text_size).value.sp,
             color = Color.White,
-            modifier = Modifier
-                .padding(10.dp)
-                .wrapContentSize()
+            maxLines = if (isTruncated.value) 1 else Int.MAX_VALUE,
+            overflow = TextOverflow.Ellipsis
         )
+        if (linesCount > 2) {
+            Text(text = if (isTruncated.value) "More" else "Hide",
+                color = Color.LightGray,
+                fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+                fontSize = 12.sp,
+                modifier = Modifier
+                    .padding(top = 5.dp)
+                    .clickable(onClick = {
+                        isTruncated.value = isTruncated.value.not()
+                    })
+            )
+        }
     }
 }
