723e329d2fa0f1bfa969db9651f97d60998c89ae
TarasovVP <vptarasov1998@gmail.com>
2024-10-29 21:23:52 +0200

    Fix merge conflicts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,12 @@ buildscript {
 }
 
 plugins {
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id("com.android.application") version "7.4.1" apply false
-    id("com.android.library") version "7.4.1" apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.composeMultiplatform) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.sqlDelight) apply false
+    alias(libs.plugins.kotlinJvm) apply false
 }
