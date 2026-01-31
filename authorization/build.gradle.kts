3c1824647a290d64bd869ca1a4d7a96de3e18c82
TarasovVP <vptarasov1998@gmail.com>
2026-01-31 11:17:28 +0200

    Add kmp android library plagin to authorization

--- authorization/build.gradle.kts
+++ authorization/build.gradle.kts
@@ -1,21 +1,16 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kmpAndroidLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.kmpSecrets)
 }
 
 kotlin {
-    androidTarget {
-        tasks.withType<KotlinJvmCompile>().configureEach {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_21)
-            }
-        }
+    android {
+        namespace = "com.vnteam.talktoai"
+        compileSdk = libs.versions.compileSdk.get().toInt()
     }
     listOf(
         iosX64(),
@@ -35,13 +30,6 @@ kotlin {
         commonMain.dependencies {
             implementation(libs.kotlinx.serialization)
             implementation(libs.kotlinx.coroutines.core)
-            implementation(libs.androidx.viewmodel.compose)
-            // Compose
-            implementation(libs.compose.runtime)
-            implementation(libs.compose.ui)
-            implementation(libs.compose.foundation)
-            implementation(libs.compose.material3)
-            implementation(libs.compose.components.resources)
             // Ktor
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
@@ -49,60 +37,31 @@ kotlin {
             implementation(libs.ktor.client.logging)
             // Koin
             implementation(libs.koin.core)
-            implementation(libs.koin.compose)
-            implementation(libs.koin.compose.viewmodel)
-            // SQLDelight
-            implementation(libs.sqldelight.coroutines.extensions)
         }
         androidMain.dependencies {
-            implementation(libs.androidx.multidex)
+            // Ktor
             implementation(libs.ktor.client.android)
-            implementation(libs.sqldelight.android.driver)
             // Koin
             implementation(libs.koin.android)
-            implementation(libs.koin.androidx.compose)
-            // Datastore
-            implementation(libs.androidx.datastore.preferences)
-            // Lottie
-            implementation(libs.lottie.compose)
             // Google Auth
             implementation(libs.play.services.auth)
         }
         iosMain.dependencies {
+            // Ktor
             implementation(libs.ktor.client.darwin)
-            implementation(libs.sqldelight.native.driver)
-            // Datastore
-            implementation(libs.androidx.datastore.preferences)
-            // Lottie
-            implementation(libs.lottie.compose)
-        }
-        nativeMain.dependencies {
-            implementation(libs.ktor.client.darwin)
-            implementation(libs.sqldelight.native.driver)
-            // Lottie
-            implementation(libs.lottie.compose)
         }
         jvmMain.dependencies {
-            implementation(libs.koin.core)
-            implementation(libs.ktor.client.java)
             implementation(libs.kotlinx.coroutines.swing)
+            // Ktor
+            implementation(libs.ktor.client.java)
         }
         jsMain.dependencies {
+            // Ktor
             implementation(libs.ktor.client.js)
         }
     }
 }
 
-android {
-    namespace = "com.vnteam.talktoai"
-    compileSdk = 36
-    defaultConfig {
-        minSdk = 28
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
-}
+
 
 
