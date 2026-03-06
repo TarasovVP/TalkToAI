495013766b17e024874cf935c721dda9d6f1956c
TarasovVP <vptarasov1998@gmail.com>
2026-03-06 19:21:31 +0200

    Aet JM versions to a single version

--- authorization/build.gradle.kts
+++ authorization/build.gradle.kts
@@ -12,7 +12,7 @@ kotlin {
     androidTarget {
         tasks.withType<KotlinJvmCompile>().configureEach {
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_21)
+                jvmTarget.set(JvmTarget.JVM_17)
             }
         }
     }
@@ -74,8 +74,8 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
