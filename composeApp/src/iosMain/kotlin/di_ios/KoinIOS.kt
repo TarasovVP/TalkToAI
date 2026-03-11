256934eb7ae0e4f2956af806e6d58e08f96f0832
TarasovVP <vptarasov1998@gmail.com>
2026-03-11 12:19:22 +0200

    Move settings to separate module

--- composeApp/src/iosMain/kotlin/di_ios/KoinIOS.kt
+++ composeApp/src/iosMain/kotlin/di_ios/KoinIOS.kt
@@ -3,8 +3,9 @@ package di_ios
 import com.vnteam.talktoai.di.appModule
 import com.vnteam.talktoai.di.aiModule
 import com.vnteam.talktoai.di.authModule
+import com.vnteam.talktoai.di.settingsModule
 import org.koin.core.context.startKoin
 
 fun doInitKoin() = startKoin {
-    modules(authModule, aiModule, appModule, iosModule)
+    modules(authModule, aiModule, settingsModule, appModule, iosModule)
 }
---
+++ settings/src/commonMain/kotlin/com/vnteam/talktoai/di/SettingsModule.kt
@@ -0,0 +1,28 @@
+package com.vnteam.talktoai.di
+
+import com.vnteam.talktoai.data.repositoryimpl.PreferencesRepositoryImpl
+import com.vnteam.talktoai.domain.repositories.PreferencesRepository
+import com.vnteam.talktoai.presentation.usecaseimpl.newUseCases.settings.IdTokenUseCase
+import com.vnteam.talktoai.presentation.usecaseimpl.newUseCases.settings.LanguageUseCase
+import com.vnteam.talktoai.presentation.usecaseimpl.newUseCases.settings.OnboardingUseCase
+import com.vnteam.talktoai.presentation.usecaseimpl.newUseCases.settings.ReviewUseCase
+import com.vnteam.talktoai.presentation.usecaseimpl.newUseCases.settings.ThemeUseCase
+import com.vnteam.talktoai.presentation.usecaseimpl.newUseCases.settings.UserEmailUseCase
+import org.koin.dsl.module
+
+val settingsModule = module {
+
+    single<PreferencesRepository> { PreferencesRepositoryImpl(get()) }
+
+    single { LanguageUseCase(get()) }
+
+    single { OnboardingUseCase(get()) }
+
+    single { ReviewUseCase(get()) }
+
+    single { ThemeUseCase(get()) }
+
+    single { UserEmailUseCase(get()) }
+
+    single { IdTokenUseCase(get()) }
+}
