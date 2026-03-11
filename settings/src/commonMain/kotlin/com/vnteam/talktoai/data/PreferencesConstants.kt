fc64d8f27aa30af75a5c1d15fb0af318b01a0796
TarasovVP <vptarasov1998@gmail.com>
2026-03-11 19:14:33 +0200

    Ad gitignore files

---
+++ settings/src/commonMain/kotlin/com/vnteam/talktoai/data/PreferencesConstants.kt
@@ -0,0 +1,11 @@
+package com.vnteam.talktoai.data
+
+const val PREFERENCES_PB = "aat.preferences_pb"
+const val IS_DARK_THEME = "isDarkTheme.db"
+const val APP_LANGUAGE = "appLanguage"
+const val IS_ONBOARDING_SEEN = "isOnboardingSeen"
+const val USER_EMAIL = "userEmail"
+const val ID_TOKEN = "idToken"
+const val IS_REVIEW_VOTE = "isReviewVote"
+const val APP_LANG_EN = "en"
+const val APP_LANG_UK = "uk"
