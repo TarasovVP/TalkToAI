c52baace1393c472875867d256d55bd1e66f6dff
TarasovVP <vptarasov1998@gmail.com>
2026-03-28 10:42:31 +0200

    Fix get model request

--- ai/src/commonMain/kotlin/com/vnteam/talktoai/data/repositoryimpl/AIRepositoryImpl.kt
+++ ai/src/commonMain/kotlin/com/vnteam/talktoai/data/repositoryimpl/AIRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.vnteam.talktoai.data.repositoryimpl
 import com.vnteam.talktoai.data.network.ai.AIService
 import com.vnteam.talktoai.data.network.ai.request.ApiRequest
 import com.vnteam.talktoai.data.network.ai.responses.ApiResponse
+import com.vnteam.talktoai.data.network.ai.responses.ModelsResponse
 import com.vnteam.talktoai.data.network.handleResponse
 import com.vnteam.talktoai.domain.repositories.AIRepository
 import kotlinx.coroutines.flow.flow
@@ -14,5 +15,10 @@ class AIRepositoryImpl(
     override suspend fun sendRequest(apiRequest: ApiRequest, apiKey: String?) = flow {
         val httpResponse = aIService.sendRequest(apiRequest, apiKey)
         emit(httpResponse.handleResponse<ApiResponse>())
+    }
+
+    override suspend fun getModels(apiKey: String?) = flow {
+        val httpResponse = aIService.getModels(apiKey)
+        emit(httpResponse.handleResponse<ModelsResponse>())
     }
 }
