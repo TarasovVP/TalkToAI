9138ad03732dd617a9bed4f29c92918d40ec0b0e
TarasovVP <vptarasov1998@gmail.com>
2026-06-16 09:00:19 +0300

    Configure db for desktop

--- shared/src/jvmMain/kotlin/com/vnteam/talktoai/data/database/DatabaseDriverFactory.kt
+++ shared/src/jvmMain/kotlin/com/vnteam/talktoai/data/database/DatabaseDriverFactory.kt
@@ -4,11 +4,18 @@ import app.cash.sqldelight.async.coroutines.awaitCreate
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import com.vnteam.talktoai.AppDatabase
+import com.vnteam.talktoai.data.DEMO_OBJECTS_DB
+import java.io.File
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class DatabaseDriverFactory {
     actual suspend fun createDriver(): SqlDriver {
-        return JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-            .also { AppDatabase.Schema.awaitCreate(it) }
+        val dbDir = File(System.getProperty("user.home"), ".talktoai").also { it.mkdirs() }
+        val dbFile = File(dbDir, DEMO_OBJECTS_DB)
+        val driver = JdbcSqliteDriver("jdbc:sqlite:${dbFile.absolutePath}")
+        if (!dbFile.exists()) {
+            AppDatabase.Schema.awaitCreate(driver)
+        }
+        return driver
     }
 }
